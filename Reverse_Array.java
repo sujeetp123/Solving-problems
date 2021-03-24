aa020a8385f4b481510954c7420c5f86418a7815
sujeet <sujeetp648@gmail.com>
2021-03-24 06:33:13 +0530

    Reverse of array

---
+++ Reverse_Array.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Reverse_Array {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+            
+           
+        }
+    
+    
+    
+    for(int i1 =arr.length-1 ; i1>=0 ;i1--){
+            System.out.print(arr[i1] + " ");
+        }
+        scanner.close();
+        
+        
+        
+    }
+}
